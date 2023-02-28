f8abfdf916ac5ede35fa1125d192163f3edbc206
MarySolidum <m.e.solidum@gmail.com>
2023-02-28 17:31:09 -0500

    starting to work on js for modal feature

--- modal-feature.js
+++ modal-feature.js
@@ -8,18 +8,19 @@ app.init = () => {
 
 // PSEUDOCODE
 
-// 2. Create a for each condition with an event listener
-// 3.  On click, modal appears
+
+
 // 4. design an exit button for use to get out of modal
 
 app.galleryListener = () => {
     // 1. Target all li elements in gallery wrapper
     const galleryImages = document.querySelectorAll('.galleryItem');
-    console.log(galleryImages);
 
+    // 2. Create a for each condition with an event listener
     galleryImages.forEach((image) => {
         image.addEventListener('click', function() {
-            console.log('clicked')
+           // 3.  On click, modal appears
+           
         })
     });
 }
