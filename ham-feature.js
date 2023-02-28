fe2b0cc6817352ba2cdeada6f99d70759b780b5a
MarySolidum <m.e.solidum@gmail.com>
2023-02-28 14:47:00 -0500

    disabled scrolling when nav menu is visble

--- ham-feature.js
+++ ham-feature.js
@@ -13,11 +13,34 @@ app.hamBtnListener = () => {
     // nav menu element saved in a variable
     const nav = document.getElementById("navMenu");
 
+    const sophieBtn = document.getElementById("sophieLogo");
+
     // listen to click of menu
     hamButton.addEventListener('click',function(){
         // add/remove class to nav menu
         nav.classList.toggle('show');
+
+        // disable scrolling when menu is visible
+        const body = document.querySelector('body');
+        if (nav.classList.contains('show') === true) {
+            body.classList.add('removeScrolling');
+        } else {
+            body.classList.remove('removeScrolling');
+        }
     })
+
+    // when sophie home button is clicked, remove nav menu
+    sophieBtn.addEventListener('click',function() {
+        nav.classList.remove('show');
+    })
+
+
+
 }
+
+// on scroll, scroll back to the top button appears
+// app.scrollListener = () => {
+    
+// }
 
 app.init();
