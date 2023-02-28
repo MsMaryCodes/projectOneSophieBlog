1cf15e35f9320013d492788dedb62a889f9cd1e1
MarySolidum <m.e.solidum@gmail.com>
2023-02-28 16:03:50 -0500

    added back to top button

--- ham-feature.js
+++ ham-feature.js
@@ -3,8 +3,11 @@ const app = {}
 
 // init method
 app.init = () => {
-    app.hamBtnListener()
+    app.hamBtnListener();
+    app.scrollListener();
 };
+
+const body = document.querySelector('body');
 
 // method to listen for click on hamburger menu button
 app.hamBtnListener = () => {
@@ -21,7 +24,6 @@ app.hamBtnListener = () => {
         nav.classList.toggle('show');
 
         // disable scrolling when menu is visible
-        const body = document.querySelector('body');
         if (nav.classList.contains('show') === true) {
             body.classList.add('removeScrolling');
         } else {
@@ -32,15 +34,23 @@ app.hamBtnListener = () => {
     // when sophie home button is clicked, remove nav menu
     sophieBtn.addEventListener('click',function() {
         nav.classList.remove('show');
+        body.classList.remove('removeScrolling');
     })
-
-
-
 }
 
 // on scroll, scroll back to the top button appears
-// app.scrollListener = () => {
+app.scrollListener = () => {
+    // target scroll to the top button
+    const scrollBtn = document.querySelector('#topBtn');
+
+    document.addEventListener("scroll", function() {
+        if (body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            scrollBtn.style.display = "flex";
+        } else {
+            scrollBtn.style.display = "none";
+        }
+    })
     
-// }
+}
 
 app.init();
