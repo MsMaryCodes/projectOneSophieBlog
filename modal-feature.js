3dac6836a6274130a06c882b75f4d222e9890284
MarySolidum <m.e.solidum@gmail.com>
2023-02-28 17:16:38 -0500

    started modal styling

--- modal-feature.js
+++ modal-feature.js
@@ -3,8 +3,26 @@ const app = {}
 
 // init method
 app.init = () => {
-
+    app.galleryListener()
 };
+
+// PSEUDOCODE
+
+// 2. Create a for each condition with an event listener
+// 3.  On click, modal appears
+// 4. design an exit button for use to get out of modal
+
+app.galleryListener = () => {
+    // 1. Target all li elements in gallery wrapper
+    const galleryImages = document.querySelectorAll('.galleryItem');
+    console.log(galleryImages);
+
+    galleryImages.forEach((image) => {
+        image.addEventListener('click', function() {
+            console.log('clicked')
+        })
+    });
+}
 
 
 app.init();
